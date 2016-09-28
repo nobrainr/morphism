0d03e8097a9992d741d59a60b94ac3c2503f3a65
Yann Renaudin <renaudin.yann@gmail.com>
2016-09-27 23:48:27 -0400

    feat(tests): coverage to 97%

--- gulpfile.js
+++ gulpfile.js
@@ -16,63 +16,63 @@ var isparta = require('isparta');
 require('babel-register');
 
 gulp.task('static', function () {
-  return gulp.src('**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+    return gulp.src('**/*.js')
+        .pipe(excludeGitignore())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
+    nsp({ package: path.resolve('package.json') }, cb);
 });
 
 gulp.task('pre-test', function () {
-  return gulp.src('lib/**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(istanbul({
-      includeUntested: true,
-      instrumenter: isparta.Instrumenter
-    }))
-    .pipe(istanbul.hookRequire());
+    return gulp.src('lib/**/*.js')
+        .pipe(excludeGitignore())
+        .pipe(istanbul({
+            includeUntested: true,
+            instrumenter: isparta.Instrumenter
+        }))
+        .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr;
+    var mochaErr;
 
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
-    .on('error', function (err) {
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', function () {
-      cb(mochaErr);
-    });
+    gulp.src('test/**/*.js')
+        .pipe(plumber())
+        .pipe(mocha({ reporter: 'spec' }))
+        .on('error', function (err) {
+            mochaErr = err;
+        })
+        .pipe(istanbul.writeReports())
+        .on('end', function () {
+            cb(mochaErr);
+        });
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
+    gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
 });
 
 gulp.task('coveralls', ['test'], function () {
-  if (!process.env.CI) {
-    return;
-  }
+    if (!process.env.CI) {
+        return;
+    }
 
-  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-    .pipe(coveralls());
+    return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+        .pipe(coveralls());
 });
 
 gulp.task('babel', ['clean'], function () {
-  return gulp.src('lib/**/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('dist'));
+    return gulp.src('lib/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function () {
-  return del('dist');
+    return del('dist');
 });
 
 gulp.task('prepublish', ['nsp', 'babel']);
--- test/index.js
+++ test/index.js
@@ -1,8 +1,96 @@
-import assert from 'assert';
-import morphism from '../lib';
+import expect from 'expect';
+import { Morphism } from '../lib';
 
 describe('morphism', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
+
+    beforeEach(function () {
+        this.dataToCrunch = [{
+            'firstName': 'John',
+            'lastName': 'Smith',
+            'age': 25,
+            'address':
+            {
+                'streetAddress': '21 2nd Street',
+                'city': 'New York',
+                'state': 'NY',
+                'postalCode': '10021'
+            },
+            'phoneNumber':
+            [
+                {
+                    'type': 'home',
+                    'number': '212 555-1234'
+                },
+                {
+                    'type': 'fax',
+                    'number': '646 555-4567'
+                }
+            ]
+        }];
+    });
+
+    describe('structural', function () {
+        it('should export Morphism function curried function', function () {
+            expect(Morphism).toBeA('function');
+        });
+
+        it('should provide a function when map configuration only is given', function () {
+            let fn = Morphism({});
+            expect(fn).toBeA('function');
+        });
+
+        it('should provide an array of results when Morphism applied on array of data', function () {
+            expect(Morphism({}, this.dataToCrunch)).toBeA('object');
+        });
+    });
+
+    describe('flat projection', function () {
+
+        beforeEach(function () {
+
+        });
+
+        it('should flatten data from specified path', function () {
+            let schema = {
+                firstName: 'firstName',
+                lastName: 'lastName',
+                city: 'address.city'
+            };
+
+            let desiredResult = {
+                firstName: 'John',
+                lastName: 'Smith',
+                city: 'New York'
+            };
+            let results = Morphism(schema, this.dataToCrunch);
+            expect(results[0]).toEqual(desiredResult);
+        });
+
+        it('should compute function on data from specified path', function () {
+            let schema = {
+                state: {
+                    path: 'address.state',
+                    fn: (s) => s.toLowerCase()
+                }
+            };
+
+            let desiredResult = {
+                state: 'ny' // from NY to ny
+            };
+            let results = Morphism(schema, this.dataToCrunch);
+            expect(results[0]).toEqual(desiredResult);
+        });
+
+        it('should set the value as default when anonymous function is provided', function () {
+            let schema = {
+                status: () => 'morphism-ed'
+            };
+
+            let desiredResult = {
+                status: 'morphism-ed'
+            };
+            let results = Morphism(schema, this.dataToCrunch);
+            expect(results[0]).toEqual(desiredResult);
+        });
+    });
 });
