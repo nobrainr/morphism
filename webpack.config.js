0457ff7c6a7fdffb7ed39c976ff1a17198d2b7e6
Yann Renaudin <renaudin.yann@gmail.com>
2018-01-09 22:58:05 -0500

    feat(CI): Fix Hang on CI

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,64 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const DashboardPlugin = require("webpack-dashboard/plugin");
-const nodeEnv = process.env.NODE_ENV || "development";
-const isProd = nodeEnv === "production";
+const webpack = require('webpack');
+const path = require('path');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = nodeEnv === 'production';
+
+const plugins = [
+    new webpack.optimize.UglifyJsPlugin({
+        compress: { warnings: false },
+        output: { comments: false },
+        sourceMap: true
+    }),
+    new webpack.LoaderOptionsPlugin({
+        options: {
+            tslint: {
+                emitErrors: true,
+                failOnHint: true
+            }
+        }
+    })
+];
+
+if(!isProd){
+    plugins.push(new DashboardPlugin());
+}
 
 var config = {
-  devtool: isProd ? "hidden-source-map" : "source-map",
-  context: path.resolve("./src"),
-  entry: {
-    app: "./index.ts"
-  },
-  output: {
-    path: path.resolve("./dist"),
-    filename: "[name].bundle.js",
-    sourceMapFilename: "[name].bundle.map",
-    devtoolModuleFilenameTemplate: function (info) {
-      return "file:///" + info.absoluteResourcePath;
-    }
-  },
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.ts?$/,
-        exclude: ["node_modules"],
-        use: ["awesome-typescript-loader", "source-map-loader"]
-      },
-      { test: /\.css$/, loaders: ["style-loader", "css-loader"] }
-    ]
-  },
-  resolve: {
-    extensions: [".ts", ".js"]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false },
-      output: { comments: false },
-      sourceMap: true
-    }),
-    new DashboardPlugin(),
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        tslint: {
-          emitErrors: true,
-          failOnHint: true
+    devtool: isProd ? 'hidden-source-map' : 'source-map',
+    context: path.resolve('./src'),
+    entry: {
+        app: './index.ts'
+    },
+    output: {
+        path: path.resolve('./dist'),
+        filename: '[name].bundle.js',
+        sourceMapFilename: '[name].bundle.map',
+        devtoolModuleFilenameTemplate: function (info) {
+            return 'file:///' + info.absoluteResourcePath;
         }
-      }
-    })
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, "dist/"),
-    compress: true,
-    port: 3000,
-    hot: true
-  }
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.ts?$/,
+                exclude: ['node_modules'],
+                use: ['awesome-typescript-loader', 'source-map-loader']
+            },
+            { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    plugins: plugins,
+    devServer: {
+        contentBase: path.join(__dirname, 'dist/'),
+        compress: true,
+        port: 3000,
+        hot: true
+    }
 };
 
 module.exports = config;
