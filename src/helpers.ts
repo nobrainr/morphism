0f214b19476c29270e3f96dee82ba570ef26d897
Yann Renaudin <renaudin.yann@gmail.com>
2019-03-22 19:17:10 -0400

    feat: add Symbol for SchemaOptions

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,7 @@
 import { ActionSelector, ActionAggregator, ActionFunction } from './types';
 
+export const SCHEMA_OPTIONS_SYMBOL = Symbol.for('SchemaOptions');
+
 export function isActionSelector<S, R>(value: any): value is ActionSelector<S, R> {
   return isObject(value) && value.hasOwnProperty('fn') && value.hasOwnProperty('path');
 }
