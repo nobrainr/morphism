aed1e20d38fafa70e2549876fffb7f4f2cf0a39f
Yann Renaudin <renaudin.yann@gmail.com>
2019-04-08 19:23:51 -0400

    chore: add jest coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   coverageReporters: ['json', 'lcov', 'text'],
-  verbose: true
+  verbose: true,
+  coverageThreshold: {
+    global: {
+      statements: 96,
+      branches: 90,
+      functions: 100,
+      lines: 96
+    }
+  }
 };
