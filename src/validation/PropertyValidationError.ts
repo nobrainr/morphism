3ec7227e2938183612265cdd6f8a225672ecfd61
Yann Renaudin <renaudin.yann@gmail.com>
2019-07-08 13:03:57 -0400

    refactor: PropertyValidationError type is now a string instead of symbol

--- src/validation/PropertyValidationError.ts
+++ src/validation/PropertyValidationError.ts
@@ -1,13 +1,15 @@
+import { getSymbolName } from '../helpers';
+
 export interface ValuePropertyValidationError {
   value: unknown;
   type: symbol;
 }
 export class PropertyValidationError extends Error {
   value: unknown;
-  type: symbol;
+  type: string;
   constructor(infos: ValuePropertyValidationError) {
     super();
     this.value = infos.value;
-    this.type = infos.type;
+    this.type = getSymbolName(infos.type);
   }
 }
