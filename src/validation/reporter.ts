ec984f720d63f05d920a3668047fedd8c208873a
Yann Renaudin <renaudin.yann@gmail.com>
2019-07-08 13:02:39 -0400

    refactor: refactored Reporter function to a Class, export formatter interface

--- src/validation/reporter.ts
+++ src/validation/reporter.ts
@@ -6,8 +6,10 @@ import { ValuePropertyValidationError } from './PropertyValidationError';
 
 export const ERRORS = Symbol('errors');
 
-export interface ValidationError extends ValuePropertyValidationError {
+export interface ValidationError {
   targetProperty: string;
+  value: unknown;
+  type: string;
 }
 
 export interface Errors extends Array<ValidationError> {}
@@ -16,19 +18,12 @@ export interface Validation {
   [ERRORS]: Errors;
 }
 
-export function targetContainsErrors(target: any): target is Validation {
+export function targetHasErrors(target: any): target is Validation {
   return target && target[ERRORS] && target[ERRORS].length > 0;
 }
-
-export function formatter(error: ValidationError) {
+export function defaultFormatter(error: ValidationError) {
   const { value, targetProperty, type } = error;
-  return `Invalid value ${value} supplied to : [⚠️ Schema With Type] at property ${targetProperty}. Expecting type ${getSymbolName(type)}`;
-}
-export function reporter(target: any) {
-  if (!targetContainsErrors(target)) return [];
-
-  const errors = target[ERRORS];
-  return errors.map(formatter);
+  return `Invalid value ${value} supplied to : [⚠️ Schema With Type] at property ${targetProperty}. Expecting type ${type}`;
 }
 
 export function parse(value: any, type: symbol) {
@@ -40,3 +35,20 @@ export function parse(value: any, type: symbol) {
     return BooleanValidator.validateAndParse(value);
   }
 }
+
+export interface Formatter {
+  (error: ValidationError): string;
+}
+
+export class Reporter {
+  constructor(private formatter: Formatter = defaultFormatter) {}
+
+  report(target: any) {
+    if (!targetHasErrors(target)) return null;
+
+    const errors = target[ERRORS];
+    return errors.map(this.formatter);
+  }
+}
+
+export const reporter = new Reporter();
