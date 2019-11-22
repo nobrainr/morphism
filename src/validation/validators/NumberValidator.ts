a5871988ecedc9b499957f514bc8c64cc52f59a6
Yann Renaudin <renaudin.yann@gmail.com>
2019-11-22 15:44:40 -0500

    refactor: NumberValidator now uses a LinkedList to apply the rules

--- src/validation/validators/NumberValidator.ts
+++ src/validation/validators/NumberValidator.ts
@@ -1,59 +1,66 @@
-import { ValidatorError } from './ValidatorError';
-import { Rule } from '../types';
+import { ValidatorValidateResult, ValidateFunction } from "../../types";
+import { LinkedList } from "./LinkedList";
+import { ValidatorOptions, Rule } from "./types";
+import { ValidatorError } from "./ValidatorError";
+import { isString } from "../../helpers";
 
-export function NumberValidator() {
-  const baseRuleName = 'number';
-  const baseRuleExpect = 'value to be typeof number';
-  const baseRuleValidate: Rule<string>['validate'] = value => {
-    const result = +value;
-    if (isNaN(result)) {
-      throw new ValidatorError({ value, expect: baseRuleExpect });
-    } else {
-      return result;
+export function NumberValidator(options: ValidatorOptions = {}) {
+  let list = new LinkedList<Rule<number>>({
+    name: "number",
+    expect: input =>
+      `Expected value to be a <number> but received <${input.value}>`,
+    validate: input => {
+      if (!options.convert) {
+        return typeof input.value === "number";
+      } else {
+        input.value = +input.value;
+        return !isNaN(input.value);
+      }
     }
-  };
-  const baseRule = {
-    name: baseRuleName,
-    expect: baseRuleExpect,
-    validate: baseRuleValidate
-  };
-  const rules = new Map<string, Rule<number>>([[baseRule.name, baseRule]]);
-  function executeRules(value: any) {
-    return [...rules.values()].reduce((acc, rule) => {
-      return rule.validate(acc);
-    }, value);
-  }
-  function addRule<T extends boolean>(rule: Rule<T>) {
-    if (rules.has(rule.name)) throw new Error(`Rule ${rule.name} has already been used`);
-    rules.set(rule.name, rule);
-  }
-  const api = {
-    min: (val: number) => {
-      const name = 'min';
-      const expect = `value to be greater or equal than ${val}`;
-      const validate: Rule<string>['validate'] = value => {
-        if (value < val) {
-          throw new ValidatorError({ value, expect });
-        }
-        return value;
-      };
+  });
 
-      addRule({ name, expect, validate });
-      return Object.assign(executeRules, api);
+  const validate: ValidateFunction = input => {
+    const result: ValidatorValidateResult = input;
+    const iterator = list.values();
+    let current = iterator.next();
+    const usedRules: { [id: string]: Rule<number> } = {};
+
+    while (!result.error && !current.done) {
+      const rule = current.value;
+      if (rule.name in usedRules) {
+        throw new Error(`Rule ${rule.name} has already been used`);
+      } else {
+        usedRules[rule.name] = rule;
+      }
+      if (!rule.validate(result)) {
+        result.error = new ValidatorError({
+          expect: isString(rule.expect) ? rule.expect : rule.expect(result),
+          value: result.value
+        });
+      }
+      current = iterator.next();
+    }
+    return result;
+  };
+
+  const rules = {
+    min: (value: any) => {
+      list.append({
+        name: "min",
+        expect: `value to be greater or equal than ${value}`,
+        validate: input => input.value >= value
+      });
+      return api;
     },
-    max: (val: number) => {
-      const name = 'max';
-      const expect = `value to be less or equal than ${val}`;
-      const validate: Rule<string>['validate'] = value => {
-        if (value > val) {
-          throw new ValidatorError({ value, expect });
-        }
-        return value;
-      };
-
-      addRule({ name, expect, validate });
-      return Object.assign(executeRules, api);
+    max: (value: any) => {
+      list.append({
+        name: "max",
+        expect: `value to be less or equal than ${value}`,
+        validate: input => input.value <= value
+      });
+      return api;
     }
   };
-  return Object.assign(executeRules, api);
+  const api = Object.assign(validate, rules);
+  return api;
 }
