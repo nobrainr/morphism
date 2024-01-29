09fe6cb9d020f6c9f99d87aa5f8f1c3f976fff69
Patrick Jones <patrickjones.pmj@gmail.com>
2024-01-29 04:21:58 -0600

    update webpack config to v5

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,3 +1,68 @@
+// import path from 'path';
+// import webpack from 'webpack';
+// import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+// import NodemonPlugin from 'nodemon-webpack-plugin';
+// import ModuleDependencyWarning from 'webpack/lib/ModuleDependencyWarning';
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+// const nodeEnv = process.env.NODE_ENV || 'development';
+// const isProd = nodeEnv === 'production';
+// const shouldAnalyzeBundle = process.env.WEBPACK_ANALYZE;
+// class IgnoreNotFoundExportPlugin {
+//   apply(compiler) {
+//     const messageRegExp = /export '.*'( \(reexported as '.*'\))? was not found in/;
+//     function doneHook(stats) {
+//       stats.compilation.warnings = stats.compilation.warnings.filter(function(warn) {
+//         if (warn instanceof ModuleDependencyWarning && messageRegExp.test(warn.message)) {
+//           return false;
+//         }
+//         return true;
+//       });
+//     }
+//     if (compiler.hooks) {
+//       compiler.hooks.done.tap('IgnoreNotFoundExportPlugin', doneHook);
+//     } else {
+//       compiler.plugin('done', doneHook);
+//     }
+//   }
+// }
+
+// const webpackconfiguration: webpack.Configuration = {
+//   mode: 'development',
+//   entry: path.resolve(__dirname, 'src', 'morphism.ts'),
+//   devtool: isProd ? 'hidden-source-map' : 'source-map',
+//   output: {
+//     filename: 'morphism.js',
+//     hashFunction: 'sha256',
+//     path: path.resolve(__dirname, 'dist'),
+//     libraryTarget: 'umd',
+//     globalObject: 'this',
+//     sourceMapFilename: 'morphism.map',
+//     library: 'Morphism',
+//   },
+//   resolve: {
+//     extensions: ['.ts', '.tsx', '.js', '.json'],
+//   },
+//   module: {
+//     rules: [{ test: /\.(ts|js)x?$/, use: ['babel-loader', 'source-map-loader'], exclude: /node_modules/ }],
+//   },
+//   plugins: [
+//     new ForkTsCheckerWebpackPlugin({
+//       async: false,
+//       typescript: {
+//         configFile: './tsconfig.json'
+//       }
+//       // checkSyntacticErrors: true,
+//       // reportFiles: ['**', '!**/*.json', '!**/__tests__/**', '!**/?(*.)(spec|test).*'],
+//       // silent: true,
+//     }),
+//     new NodemonPlugin(),
+//     new IgnoreNotFoundExportPlugin(),
+//     shouldAnalyzeBundle ? new BundleAnalyzerPlugin({ generateStatsFile: true }) : null,
+//   ].filter(plugin => plugin),
+// };
+
+// export default webpackconfiguration;
 import path from 'path';
 import webpack from 'webpack';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
@@ -8,11 +73,12 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 const nodeEnv = process.env.NODE_ENV || 'development';
 const isProd = nodeEnv === 'production';
 const shouldAnalyzeBundle = process.env.WEBPACK_ANALYZE;
+
 class IgnoreNotFoundExportPlugin {
   apply(compiler) {
     const messageRegExp = /export '.*'( \(reexported as '.*'\))? was not found in/;
     function doneHook(stats) {
-      stats.compilation.warnings = stats.compilation.warnings.filter(function(warn) {
+      stats.compilation.warnings = stats.compilation.warnings.filter(function (warn) {
         if (warn instanceof ModuleDependencyWarning && messageRegExp.test(warn.message)) {
           return false;
         }
@@ -27,12 +93,13 @@ class IgnoreNotFoundExportPlugin {
   }
 }
 
-const webpackconfiguration: webpack.Configuration = {
+const webpackConfiguration: webpack.Configuration = {
   mode: 'development',
   entry: path.resolve(__dirname, 'src', 'morphism.ts'),
   devtool: isProd ? 'hidden-source-map' : 'source-map',
   output: {
     filename: 'morphism.js',
+    hashFunction: 'sha256',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd',
     globalObject: 'this',
@@ -48,14 +115,14 @@ const webpackconfiguration: webpack.Configuration = {
   plugins: [
     new ForkTsCheckerWebpackPlugin({
       async: false,
-      checkSyntacticErrors: true,
-      reportFiles: ['**', '!**/*.json', '!**/__tests__/**', '!**/?(*.)(spec|test).*'],
-      silent: true,
+      typescript: {
+        configFile: './tsconfig.json',
+      },
     }),
     new NodemonPlugin(),
     new IgnoreNotFoundExportPlugin(),
-    shouldAnalyzeBundle ? new BundleAnalyzerPlugin({ generateStatsFile: true }) : null,
-  ].filter(plugin => plugin),
+    shouldAnalyzeBundle ? new BundleAnalyzerPlugin({ generateStatsFile: true }) : undefined,
+  ].filter((plugin) => plugin),
 };
 
-export default webpackconfiguration;
+export default webpackConfiguration;
